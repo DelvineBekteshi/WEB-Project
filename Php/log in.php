6df6ec27b2354b97faaff6922de908b1fa709440
DelvineBekteshi <db70675@ubt-uni.net>
2025-02-02 18:18:03 +0100

    updates

--- Php/log in.php
+++ Php/log in.php
@@ -60,7 +60,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             </div>
             <div class="remember-forgot">
                 <label><input type="checkbox" name="remember" <?php if(isset($_COOKIE['userName'])) echo "checked";?>>Remember Me</label> 
-                <a href="#">Forgot Password</a>
             </div>
             <button type="submit" name="submit" class="btn">Login</button>
             <div class="register-link">
