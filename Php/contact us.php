47a1ec3610c281f1001d89fb8d402edee5110417
FlandraBytyqi <flandra.bytyqi05@gmail.com>
2025-02-02 19:00:43 +0100

    Update contact us.php

--- Php/contact us.php
+++ Php/contact us.php
@@ -36,7 +36,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </style>
     </head>
     <body>
-    <a href="index.php">Go back to <b>Home Page</b></a> 
         <selection class="contact">
             <div class="content">
                 <h2>Contact Us</h2>
